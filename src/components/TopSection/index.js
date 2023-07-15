5d183380c05fda27a83e6590ace679267464522b
Veronika Rosicova <veronika@Veronikas-Air.broadband>
2023-07-15 18:52:00 +0100

    add top section

---
+++ src/components/TopSection/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const TopSection = () => {
+  return (
+    <div style={styles.grid} aria-label='Filter section'>
+      <h2 style={styles.heading}>Filter by</h2>
+      <div style={styles.container}>
+        <select style={styles.sort} aria-label='Sort by'>
+          <option value='recommended'>Recommended</option>
+          <option value='reviews'>Review: high to low</option>
+        </select>
+        <div style={styles.results} aria-live='polite'>
+          100 results
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopSection;
+
+const styles = {
+  grid: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 3fr',
+    gap: 20,
+  },
+  heading: {
+    fontSize: 26,
+    lineHeight: '22px',
+    margin: 0,
+    paddingBottom: 20,
+    textAlign: 'left',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    alignSelf: 'start',
+  },
+  sort: {
+    padding: 4,
+  },
+};
